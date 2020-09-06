8af4dffca1924f2280f9949609f4bd769a4d8305
dyqu1748 <47007591+dyqu1748@users.noreply.github.com>
2020-09-06 14:38:45 -0600

    Create Classes.java
    Just setting up the classes and their methods. Will most likely need to change the encapsulation of the names of the animals to meet requirements.

---
+++ Classes.java
@@ -0,0 +1,172 @@
+public class Animal{
+    
+    public void wakeUp(){
+        
+    }
+    
+    public void makeNoise(){
+        
+    }
+    
+    public void eat(){
+        
+    }
+    
+    public void roam(){
+        
+    }
+    
+    public void sleep(){
+        
+    }
+    
+}
+
+public class Feline extends Animal{
+    
+}
+
+public class Tiger extends Feline{
+    String name;
+    
+    public Tiger(){
+        name = "Tony";
+    }
+    
+}
+
+public class Cat extends Feline{
+    String name;
+    
+    public Cat(){
+        name = "Charlie";
+    }
+    
+}
+
+public class Lion extends Feline{
+    String name;
+    
+    public Lion(){
+        name = "Leo";
+    }
+}
+
+public class Pachyderm extends Animal{
+    
+}
+
+public class Rhino extends Pachyderm{
+    String name;
+    
+    public Rhino(){
+        name = "Robbie";
+    }
+    
+}
+
+public class Hippo extends Pachyderm{
+    String name;
+    
+    public Hippo(){
+        name = "Harry";
+    }
+    
+}
+
+public class Elephant extends Pachyderm{
+    String name;
+    
+    public Elephant(){
+        name = "Ellie";
+    }
+    
+}
+
+public class Canine extends Animal{
+    
+}
+
+public class Dog extends Canine{
+    String name;
+    
+    public Dog(){
+        name = "Doug";
+    }
+    
+}
+
+public class Wolf extends Canine{
+    String name;
+    
+    public Wolf(){
+        name = "Willis";
+    }
+    
+}
+
+public class Cervidae extends Animal{
+    
+}
+
+public class Moose extends Cervidae{
+    String name;
+    
+    public Moose(){
+        name = "Manny";
+    }
+}
+
+public class Caribou extends Cervidae{
+    String name;
+    
+    public Caribou(){
+        name = "Carl";
+    }
+}
+
+public class ZooEmployee{
+    
+    public void wakeAnimals(){
+        
+    }
+    
+    public void rollCall(){
+        
+    }
+    
+    public void feed(){
+        
+    }
+    
+    public void exercise(){
+        
+    }
+    
+    public void sleep(){
+        
+    
+}
+
+public class ZooKeeper extends ZooEmployee{
+    
+    public void wakeAnimals(){
+        
+    }
+    
+    public void rollCall(){
+        
+    }
+    
+    public void feed(){
+        
+    }
+    
+    public void exercise(){
+        
+    }
+    
+    public void sleep(){
+        
+    }
+}
